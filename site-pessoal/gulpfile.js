73f73c7bc4cacd272c83153d949492e463f41ab5
Andre Felizardo <andreluis.felizardo@outlook.com>
2017-09-15 13:23:34 -0300

    Carregamento de posts dinamicamente direto do blog. OGImage

--- site-pessoal/gulpfile.js
+++ site-pessoal/gulpfile.js
@@ -5,12 +5,14 @@ var watch = require('gulp-watch');
 var cssmin = require('gulp-cssmin');
 var concat = require('gulp-concat');
 var imagemin = require('gulp-imagemin');
+var livereload = require('gulp-livereload');
 
 gulp.task('minifyScripts', function() {
     return gulp
         .src(['scripts/**/*.js'])
         .pipe(uglify())
-        .pipe(gulp.dest('dist/js'));
+        .pipe(gulp.dest('dist/js'))
+        .pipe(livereload());
 });
 
 gulp.task('minifyCss', function() {
@@ -18,7 +20,8 @@ gulp.task('minifyCss', function() {
         .src(['./css/reset.css', './css/*css'])
         .pipe(concat('style.min.css'))
         .pipe(cssmin())
-        .pipe(gulp.dest('dist/styles'));
+        .pipe(gulp.dest('dist/styles'))
+        .pipe(livereload());
 });
 
 gulp.task('compressImages', function() {
@@ -28,9 +31,19 @@ gulp.task('compressImages', function() {
     .pipe(gulp.dest('dist/imgs'));
 });
 
-gulp.task('watchScripts', function() {
+gulp.task('watch', function() {
     gulp.watch('scripts/**/*.js', function(event) {
         gutil.log('File ' + event.path + ' was ' + event.type + ', running tasks...');
         gulp.run('minifyScripts');
+    })
+
+    gulp.watch('css/**/*.css', function(event) {
+        gutil.log('File ' + event.path + ' was ' + event.type + ', running tasks...');
+        gulp.run('minifyCss');
+    })
+
+    gulp.watch(['imgs/**/*.png', 'imgs/**/*.jpg'], function(event) {
+        gutil.log('File ' + event.path + ' was ' + event.type + ', running tasks...');
+        gulp.run('compressImages');
     })
 });
---
+++ site-pessoal/scripts/ultimosPosts.js
@@ -0,0 +1,18 @@
+$(document).ready(function(){
+    $.get( "http://www.andrefelizardo.com.br/blog/feed/", function( data ) {
+        for(i = 0; i < 3; i++) {
+            var titulo = $(data).find('item').eq(i).find('title').text();
+            var url = $(data).find('item').eq(i).find('link').text();
+            $("<li><a href='" + url + "'>" + titulo + "</a>").appendTo($('#ultimos-posts ul'));
+        }
+      })
+      .done(function(){
+        $('#ultimos-posts').slideToggle();
+      })
+      .fail(function() {
+
+      })
+      .always(function() {
+          $('.loadPosts').hide();
+      });
+});
