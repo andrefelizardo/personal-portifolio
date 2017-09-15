99bf4042ca698739d4651bbf8e26d2229caeef50
Andre Felizardo <andreluis.felizardo@outlook.com>
2017-09-15 03:54:30 -0300

    Comprimindo imagens e concatenando CSS

--- site-pessoal/gulpfile.js
+++ site-pessoal/gulpfile.js
@@ -2,12 +2,30 @@ var gulp = require('gulp');
 var gutil = require('gulp-util');
 var uglify = require('gulp-uglify');
 var watch = require('gulp-watch');
+var cssmin = require('gulp-cssmin');
+var concat = require('gulp-concat');
+var imagemin = require('gulp-imagemin');
 
 gulp.task('minifyScripts', function() {
     return gulp
         .src(['scripts/**/*.js'])
         .pipe(uglify())
-        .pipe(gulp.dest('build/js'));
+        .pipe(gulp.dest('dist/js'));
+});
+
+gulp.task('minifyCss', function() {
+    return gulp
+        .src(['./css/reset.css', './css/*css'])
+        .pipe(concat('style.min.css'))
+        .pipe(cssmin())
+        .pipe(gulp.dest('dist/styles'));
+});
+
+gulp.task('compressImages', function() {
+    return gulp
+    .src(['imgs/**/*.png', 'imgs/**/*.jpg'])
+    .pipe(imagemin({optimizationLevel: 5, progressive: true, interlaced: true}))
+    .pipe(gulp.dest('dist/imgs'));
 });
 
 gulp.task('watchScripts', function() {
